848b9cc2b73018c78847bb4a8ececb5769b65ff7
Agnish1611 <144105275+Agnish1611@users.noreply.github.com>
2023-09-25 00:39:50 +0530

    Update script.js

--- items_page/script.js
+++ items_page/script.js
@@ -1,3 +1,5 @@
+var i=9;
+
 document.querySelector("#btn").addEventListener("click",function(){
     document.querySelector(".sidebar").classList.toggle("active");
     document.querySelector("table").classList.toggle("active");
@@ -17,10 +19,14 @@ document.querySelector("#form-close").addEventListener("click",function(){
 });
 
 document.querySelector("#form-enter").addEventListener("click",function(){
+    document.querySelector("#item-"+i).classList.add("active");
+
     document.querySelector(".form-container").classList.remove("show");
     document.querySelector("body").classList.remove("active");
     document.querySelector(".items-list").classList.remove("active");
-    addTableRow();
+    
+    i++;
 }); 
+
 
 
